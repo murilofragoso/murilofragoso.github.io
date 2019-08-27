b495c0bcfb8c5851d0df1309bcf7dac902ab7c17
Murilo Fragoso <murilo.fragoso@smn.com.br>
2019-08-27 16:55:13 -0300

    Criando informações iniciais

---
+++ js/header.js
@@ -0,0 +1,13 @@
+function login(){
+    /*$("#divContent").load("./view/login.html"); 
+    console.log("Yes")*/
+
+    /*$.ajax({
+        url: "view/login.html",
+        type: 'GET',
+        crossDomain: true,
+        success: function(res) {
+            console.log(res)
+        }
+    });*/
+}
